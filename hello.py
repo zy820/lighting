4dbc4769756c4b6dbef7dde8e78cb119baaa42b6
zy820 <378850782@qq.com>
2018-05-25 17:11:21 +0800

    Add files via upload

--- hello.py
+++ hello.py
@@ -7,11 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    '''
     a = 1
     b = 0
     c = a/b
     return c
-    #return 'Hello World!'
+	'''
+    return 'Hello World!'
+
+@app.route('/user/<username>')
+def show_username(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_postid(post_id):
+    return 'Post %d' % post_id
 
 if __name__ == '__main__':
     app.run(debug=True)
